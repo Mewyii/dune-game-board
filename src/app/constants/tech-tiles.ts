11433885b89faa80185063abd227b6a2b3d6e256
Sebastian Quendt <sebastian.quendt@outlook.de>
2024-04-02 23:27:23 +0200

    Slightly reduced AI tech tile value.

--- src/app/constants/tech-tiles.ts
+++ src/app/constants/tech-tiles.ts
@@ -27,51 +27,51 @@ export const techTiles: TechTile[] = [
     name: { de: 'Shuttle-Flotte', en: 'Shuttle Fleet' },
     costs: 6,
     imageUrl: '/assets/images/tech-tiles/shuttle_fleet.png',
-    aiEvaluation: (player, gameState) => 0.9,
+    aiEvaluation: (player, gameState) => 0.8,
   },
   {
     name: { de: 'Holo-Projektoren', en: 'Holo Projectors' },
     costs: 3,
     imageUrl: '/assets/images/tech-tiles/holo_projectors.png',
-    aiEvaluation: (player, gameState) => 0.75 - 0.025 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.7 - 0.025 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Minimik-Film', en: 'Minimic Film' },
     costs: 2,
     imageUrl: '/assets/images/tech-tiles/minimic_film.png',
-    aiEvaluation: (player, gameState) => 0.75,
+    aiEvaluation: (player, gameState) => 0.7,
   },
   {
     name: { de: 'Entsorgungsanlage', en: 'Disposal Facility' },
     costs: 3,
     imageUrl: '/assets/images/tech-tiles/disposal_facility.png',
-    aiEvaluation: (player, gameState) => 0.75 - 0.05 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.5 - 0.05 * (gameState.currentTurn - 1) + (player.hasCouncilSeat ? 0.3 : 0.0),
   },
   {
     name: { de: 'Windfallen', en: 'Windtraps' },
     costs: 2,
     imageUrl: '/assets/images/tech-tiles/wind_traps.png',
-    aiEvaluation: (player, gameState) => 0.75 - 0.033 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.7 - 0.05 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Detonationsgeräte', en: 'Detonation Devices' },
     costs: 3,
     imageUrl: '/assets/images/tech-tiles/detonation_devices.png',
     aiEvaluation: (player, gameState) =>
-      (gameState.playerCombatUnits.shipsInGarrison > 0 ? 0.66 : 0.1) + 0.033 * (gameState.currentTurn - 1),
+      (gameState.playerCombatUnits.shipsInGarrison > 0 ? 0.6 : 0.1) + 0.25 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Trainingsdrohnen', en: 'Training Drones' },
     costs: 3,
     imageUrl: '/assets/images/tech-tiles/training_drones.png',
-    aiEvaluation: (player, gameState) => 0.66 - 0.025 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.6 - 0.025 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Memocorder', en: 'Memocorders' },
     costs: 2,
     imageUrl: '/assets/images/tech-tiles/memocorders.png',
     aiEvaluation: (player, gameState) =>
-      0.5 +
+      0.4 +
       0.025 * gameState.playerScore.bene +
       0.025 * gameState.playerScore.emperor +
       0.025 * gameState.playerScore.fremen +
@@ -81,37 +81,37 @@ export const techTiles: TechTile[] = [
     name: { de: 'Artillerie', en: 'Artillery' },
     costs: 1,
     imageUrl: '/assets/images/tech-tiles/artillery.png',
-    aiEvaluation: (player, gameState) => 0.5,
+    aiEvaluation: (player, gameState) => 0.4,
   },
   {
     name: { de: 'Invasionschiffe', en: 'Invasion Ships' },
     costs: 5,
     imageUrl: '/assets/images/tech-tiles/invasion_ships.png',
     aiEvaluation: (player, gameState) =>
-      (gameState.playerCombatUnits.troopsInGarrison < 3 ? 0.33 : 0.1) + 0.025 * (gameState.currentTurn - 1),
+      (gameState.playerCombatUnits.troopsInGarrison < 3 ? 0.3 : 0.0) + 0.05 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Chaumurky', en: 'Chaumurky' },
     costs: 1,
     imageUrl: '/assets/images/tech-tiles/chaumurky.png',
-    aiEvaluation: (player, gameState) => 0.1 + 0.05 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.1 + 0.033 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Limitierte Kampfmittel', en: 'Restricted Ordnance' },
     costs: 4,
     imageUrl: '/assets/images/tech-tiles/restricted_ordnance.png',
-    aiEvaluation: (player, gameState) => (player.hasCouncilSeat ? 0.9 : 0.1),
+    aiEvaluation: (player, gameState) => (player.hasCouncilSeat ? 0.8 : 0.1),
   },
   {
     name: { de: 'Holtzmann-Antrieb', en: 'Holtzman Engine' },
     costs: 6,
     imageUrl: '/assets/images/tech-tiles/holtzman_engine.png',
-    aiEvaluation: (player, gameState) => 0.66 + 0.025 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.6 + 0.025 * (gameState.currentTurn - 1),
   },
   {
     name: { de: 'Flaggschiff', en: 'Flagship' },
     costs: 8,
     imageUrl: '/assets/images/tech-tiles/flagship.png',
-    aiEvaluation: (player, gameState) => 0.33 + 0.05 * (gameState.currentTurn - 1),
+    aiEvaluation: (player, gameState) => 0.2 + 0.066 * (gameState.currentTurn - 1),
   },
 ];
